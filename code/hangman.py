30ac1a41d6ca5013f48b0cc699e87085c2ecc279
TheBlueNeon <marius.poncon@gmail.com>
2023-09-26 12:42:07 +0100

    update

--- code/hangman.py
+++ code/hangman.py
@@ -2,7 +2,6 @@ import random
 #put the list countaining a lot of words
 hangman_list = open('wordlist.txt').read().splitlines() # reads the file "wordlist.txt" and put it as a list
 shuffled_word = random.choice(hangman_list) # take at random a word from the list
-
 word_lenght = len(shuffled_word) # counts the lenght of the word which has been picked at random
 
 tries = 0 # the number of total guesses from the user, starting at 0
@@ -14,7 +13,9 @@ def ask():
     global tries
     ask_letter = str(input("Enter a letter : ")) # asking the user to guess a leter
 
-#    if ask_letter: # looking for the input to be no more than 1 letter
-
+    if (len(ask_letter)) != 1: # looking for the input to be no more than 1 letter
+        print("needs to be only one letter")
+    else:
+        print("idk yet")
 
 ask()
--- instructions.md
+++ instructions.md
@@ -8,7 +8,6 @@ Project
 
 # Breaking down problems
 
-
 ### Random
 - [x] - create a word list:
 - [x] - pick at random a word from that list and use it as the word to guess
@@ -20,3 +19,4 @@ Project
 ### User
 - [x] - tell to the user how many letters there are in the word 
 - [ ] - put the guessing into a function
+- [ ] - if the user puts more than 1 letter, it returns an error and re asked him
