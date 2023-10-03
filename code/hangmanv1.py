f8a5969d24763c6b05bf6c0a1dafe9fd4f0ea3a1
TheBlueNeon <marius.poncon@gmail.com>
2023-10-03 19:55:45 +0100

    Update hangmanv1.py

--- code/hangmanv1.py
+++ code/hangmanv1.py
@@ -7,7 +7,7 @@ alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
 tries = 1
 wrongguessedword = []
 
-HANGMANPICS = ["",'''
+HANGMANPICS = ['','''
   +---+
   |   |
       |
